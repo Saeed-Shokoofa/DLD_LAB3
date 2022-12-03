e26932c81a6ce5343383ebe8fcb9030fee4bf7b2
s_shokoofa <sd3power@gmail.com>
2022-12-03 15:08:08 +0330

    DDS and its memory added

---
+++ DDS.v
@@ -0,0 +1,11 @@
+module DDS(clk,phase_ctrl,DDS_out);
+input clk;
+input [7:0] phase_ctrl;
+output [7:0] DDS_out;
+reg [7:0]adr=0;
+always @(posedge clk) begin
+    adr=adr+phase_ctrl;
+end
+DDS_mem DM(adr,clk,DDS_out);
+
+endmodule
