754cb5fd7382a170e6f86aa6e7705a9543817904
s_shokoofa <sd3power@gmail.com>
2022-12-03 12:54:22 +0330

    triangle,square,reciprocal added

--- wave_gen.v
+++ wave_gen.v
@@ -2,15 +2,26 @@ module wave_gen(clk,phase_ctrl,func,wave);
 input clk;
 input [7:0] phase_ctrl;
 input [2:0] func;
-output [8:0]wave;
+output [7:0]wave;
+reg [15:0]n=0;
+wire [7:0] triangle,square,reciprocal;
+assign triangle=(n[15]==1'b1)?{~(n[14:7])}:{n[14:7]};
+assign square=(n[15]==1'b1)?8'b11111111:8'b00000000;
+wire [6:0]for_rec;
+assign for_rec=(n[14:11]*n[14:11])>>1+n[14:11];
+assign reciprocal={n[15],for_rec};
+always @(posedge clk)  begin
+    n=n+1;
+end
+
 always @(*) begin
     case(func)
-    3'b000:
-    3'b001:
-    3'b010:
-    3'b011:
-    3'b100:
-    3'b101:
-    3'b110:
+    3'b000:wave=reciprocal;
+    3'b001:wave=square;
+    3'b010:wave=triangle;
+    3'b011:wave=sine;
+    3'b100:wave=full_rec;
+    3'b101:wave=half_rec;
+    3'b110:wave=DDS;
 end
 endmodule
